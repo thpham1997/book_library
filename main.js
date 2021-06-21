4331c995688569bf57361c787fb29db801793700
TP1997 <thanhphamlike@gmail.com>
2021-06-20 23:19:49 -0400

    Added some basic functions

---
+++ main.js
@@ -0,0 +1,76 @@
+const BOOKLIST = document.querySelector('.book_list');
+const ADDING_CARD = document.querySelector('.adding_card');
+const FORM = document.querySelector('.form');
+const FORM_BTN_CLOSE = document.querySelector('.form__btn--close')
+const FORM_BTN_SUBMIT = document.querySelector('.form__btn--submit');
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
+  }
+}
+
+Book.prototype.isRead = function () {
+  this.read = !this.read;
+}
+
+function addBookToLibrary(book) {
+  if (!myLibrary.includes(book)) {
+    book['index'] = myLibrary.length;
+    myLibrary.push(book);
+    return true;
+  }
+  return false;
+}
+
+// manually added books
+
+const book1 = new Book('The Information: A History, a Theory, a Flood', 'James Gleick', 200, false);
+const book2 = new Book('Everything Is Miscellaneous: The Power of the New Digital Disorder', 'David Weinberger', 300, true);
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+
+ADDING_CARD.addEventListener('click', (e) => {
+  FORM.style.display = 'block';
+});
+FORM_BTN_CLOSE.addEventListener('click', (e) => {
+  FORM.style.display = 'none';
+})
+FORM_BTN_SUBMIT.addEventListener('click', (e) => {
+  console.log(FORM.element.value);
+});
+function makeCard() {
+  myLibrary.forEach((elm) => {
+    let card = document.createElement('div');
+    let p = document.createElement('p');
+    let deleteBtn = document.createElement('button');
+    let readBtn = document.createElement('button');
+
+    // deleteBtn.classList.add('deleteBtn')
+    // deleteBtn.style.display = 'none';
+    readBtn.addEventListener('click', (e) => {
+      elm.isRead();
+      p.textContent = elm.info();
+    });
+    deleteBtn.addEventListener('click', (e) => {
+      myLibrary.splice(elm.index, 1);
+      BOOKLIST.removeChild(card);
+    });
+    p.textContent = elm.info();
+    deleteBtn.textContent = 'Delete';
+    readBtn.textContent = 'Read';
+    p.style.padding = '1rem 1rem';
+    card.appendChild(p);
+    card.appendChild(deleteBtn);
+    card.appendChild(readBtn);
+    card.classList.add('card');
+    BOOKLIST.insertBefore(card, ADDING_CARD);
+  });
+}
+makeCard();
